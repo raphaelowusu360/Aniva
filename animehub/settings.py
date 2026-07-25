14ff76c542c4ada2b625b338a373573e4ed4268a
Raphael <raphael205owusu@gmail.com>
2026-07-25 15:08:48 +0000

    Update allowed hosts

--- animehub/settings.py
+++ animehub/settings.py
@@ -17,7 +17,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -----------------------------
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "aniva.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # -----------------------------
