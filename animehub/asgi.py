6524ce491efeb6821bea10c3b290e4f55bcdef39
Raphael <raphael205owusu@gmail.com>
2026-08-14 14:08:33 +0000

    Fix ASGI WebSocket configuration

--- animehub/asgi.py
+++ animehub/asgi.py
@@ -1,3 +1,12 @@
+"""
+ASGI config for animehub project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see:
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
 import os
 
 from django.core.asgi import get_asgi_application
