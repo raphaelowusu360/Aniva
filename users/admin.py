2ce542886361d49535246b18710c5624d48e4f1b
Raphael <raphael205owusu@gmail.com>
2026-08-14 11:10:17 +0000

    Image preview

--- users/admin.py
+++ users/admin.py
@@ -12,3 +12,42 @@ class GroupPostAdmin(admin.ModelAdmin):
     list_display = ("group", "author", "created_at")
     search_fields = ("group__name", "author__username")
     list_filter = ("created_at",)
+
+
+from django.contrib import admin
+from .models import FeatureFeedback, FeedbackReaction
+
+
+@admin.register(FeatureFeedback)
+class FeatureFeedbackAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "user",
+        "feature_request",
+        "created_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "feature_request",
+        "likes_about_site",
+    )
+
+    ordering = (
+        "-created_at",
+    )
+
+
+@admin.register(FeedbackReaction)
+class FeedbackReactionAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "user",
+        "feedback",
+        "reaction",
+        "created_at",
+    )
+
+    list_filter = (
+        "reaction",
+    )
